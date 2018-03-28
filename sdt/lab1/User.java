e23dd58dddc0e1a09e630c11d0a9a2e0d14fc214
Fedia Andrienko <andrienko.kpi@gmail.com>
2018-03-29 01:24:02 +0300

    Create User.java

---
+++ sdt/lab1/User.java
@@ -0,0 +1,13 @@
+public class User {
+    String login;
+    String name;
+    String surname;
+    String password;
+
+    public User(String nlogin, String nname, String nsurname, String npassword){
+        login = nlogin;
+        name = nname;
+        surname = nsurname;
+        password = npassword;
+    }
+}
